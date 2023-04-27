5fc86b6cda4bdfb6d1e0560f9fa6c61ac25aec97
Rana Saha <ranasaha03edu@gmail.com>
2023-04-27 21:52:02 +0530

    partials

--- index.js
+++ index.js
@@ -3,10 +3,24 @@ const path = require("path");
 const app = express();
 const PORT = 8000;
 
+// for partials
+const hbs = require("hbs");
+
+// set partials path
+const dir = path.dirname(__filename);
+const partialspath = path.join(dir, "/partials");
+
 // // builtin middleware
 // const staticPath = path.join('_dirname', "");
 
+/**
+ * Partials is just like templating in laravel to separate headers & footers & other common parts of a website
+*/
+
 // load Routers
 app.use('/', require('./server/routes/router'))
+
+// register the partials path
+hbs.registerPartials(partialspath);
 
 app.listen(PORT, ()=>{ console.log("Server is running on port : 8000" ) })
